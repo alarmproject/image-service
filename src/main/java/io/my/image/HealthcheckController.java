40bf7452b2ef8aca55adaae20f7db6988a291f5f
mysend12 <mysend12@naver.com>
2022-05-30 21:17:51 +0900

    DB연동 코드 추가

--- src/main/java/io/my/image/HealthcheckController.java
+++ src/main/java/io/my/image/HealthcheckController.java
@@ -1,14 +1,22 @@
 package io.my.image;
 
+import io.my.image.image.ImageRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
+@RequiredArgsConstructor
 public class HealthcheckController {
+    private final ImageRepository imageRepository;
 
     @GetMapping("/healthcheck")
     public ResponseEntity<String> healthcheck() {
+        log.info("is it {}", imageRepository.findById(0L));
+
         return ResponseEntity.ok("image service health");
     }
 }
